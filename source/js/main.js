43eb36ff88ca501f3404e25c35fc7d600b9e6982
songhn233 <47357585+songhn233@users.noreply.github.com>
2020-07-15 21:14:21 +0800

    add: iconfont and scroll-up-animate

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,11 @@
-// function to control scroll-up
-function smoothScrollToTop() {
-  let yTopValve = (window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop);
-  if (yTopValve > 1) {
-    window.requestAnimationFrame(smoothScrollToTop);
-    scrollTo(0, Math.floor(yTopValve * 0.85));
-  } else {
-    scrollTo(0, 0);
-  }
-}
-document.getElementById('scrollbutton').onclick = smoothScrollToTop;
+// // function to control scroll-up
+// function smoothScrollToTop() {
+//   let yTopValve = (window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop);
+//   if (yTopValve > 1) {
+//     window.requestAnimationFrame(smoothScrollToTop);
+//     scrollTo(0, Math.floor(yTopValve * 0.85));
+//   } else {
+//     scrollTo(0, 0);
+//   }
+// }
+// document.getElementById('scrollbutton').onclick = smoothScrollToTop;
