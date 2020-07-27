a69b8c4ed154284bc16256672a531f3e58f68cba
songhn233 <47357585+songhn233@users.noreply.github.com>
2020-07-27 16:53:49 +0800

    refactor: lazyload

--- scripts/functions/configMerge.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-'use strict';
-hexo.on('generateBefore', function () {
-  if (hexo.locals.get) {
-    const data = hexo.locals.get('data');
-    data && data.kaze_config && (hexo.theme.config = data.kaze_config);
-  }
-});
--- scripts/highlight/replace.js
+++ scripts/highlight/replace.js
@@ -1,8 +1,7 @@
 /* eslint-disable */
 'use strict';
-hexo.extend.filter.register('after_render:html', (data) => {
+hexo.extend.filter.register('after_post_render', (data) => {
   // delete <figure>, remain the <pre><code>...</code></pre>
   // $4 is the fourth matchment in the RegExp below, exactly the code block
-  data = data.replace(/<figure(.*?)<table>(.*?)<\/pre>(.*?)<pre>(.*?)<\/pre><\/td>(.*?)<\/table><\/figure>/g, '<pre class="highlight">$4</pre>')
-  return data
+  data.content = data.content.replace(/<figure(.*?)<table>(.*?)<\/pre>(.*?)<pre>(.*?)<\/pre><\/td>(.*?)<\/table><\/figure>/g, '<pre class="highlight">$4</pre>');
 });
